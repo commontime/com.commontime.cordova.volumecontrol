dddf8a544ada4cf322f5dcba59e85d24724001d9
Richard Lewin <>
2019-01-31 15:57:16 +0000

    Added missing comma.

--- www/volumecontrol.js
+++ www/volumecontrol.js
@@ -3,7 +3,7 @@
 module.exports = {
     setDeviceVolume: function (successCallback, errorCallback, volume, stream) {
         cordova.exec(successCallback, errorCallback, "VolumeControl", "setDeviceVolume", [volume, stream]);
-    }
+    },
     getDeviceVolume: function (successCallback, errorCallback, volume, stream) {
         cordova.exec(successCallback, errorCallback, "VolumeControl", "getDeviceVolume", [volume, stream]);
     }
